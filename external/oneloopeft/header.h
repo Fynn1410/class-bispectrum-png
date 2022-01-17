a96ad83758c4ad8e45752d125ff04517b96072ff
Dennis Linde <dennis.linde@rwth-aachen.de>
2022-01-17 10:51:13 +0100

    integrating_limHaloPT

---
+++ external/oneloopeft/header.h
@@ -0,0 +1,170 @@
+
+
+/** @file header.h
+ * 
+ */
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#define _GNU_SOURCE
+
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+#include <string.h>
+#include <omp.h>
+//#include <mpi.h>
+#include <ctype.h>
+#include "/home/dennis/Software/class/include/class.h"
+
+
+
+#define PSC  		101L
+#define ST 		    	102L
+#define TR 		    	103L
+
+#define GROWTH		104L
+#define DERGROWTH		105L
+
+#define NONLINEAR	   	106L
+#define LINEAR		107L
+
+#define GAUSSIAN		114L
+#define NONGAUSSIAN	115L
+
+#define INIT  		116L
+#define LOCAL		117L
+#define EQUILATERAL	118L
+#define ORTHOGONAL	119L
+#define QSF			120L
+#define HS			121L
+#define NGLOOP		122L
+#define derNGLOOP		123L
+
+#define QUADRATIC		124L
+#define TIDE		125L
+#define GAMMA		126L
+
+#define LPOWER		127L
+#define NLPOWER		128L
+#define TRANS		129L
+#define DER 		130L
+
+#define CO10  		131L
+#define CO21  		132L
+#define CO32  		133L
+#define CO43  		134L
+#define CO54  		135L
+#define CO65  		136L
+#define CII 		137L
+
+#define MATTER		138L
+#define LINEMATTER	139L
+#define LINE 		140L
+
+#define DST			141L	
+#define GFILTER		142L
+#define BSPLINE		143L
+
+
+#define TREE		144L	
+#define LOOP		145L
+#define WIR			146L
+#define NOIR		147L
+
+#define HALO 		148L
+
+
+#define PS_KMIN        1.0e-7
+#define PS_KMAX        1.0e4
+
+#define CLEANUP        1 
+
+#define DO_NOT_EVALUATE -1.0
+
+#define MAXL 2000
+
+/** 
+ * List of limHaloPT header files
+ */
+#include "Global_Structs.h"
+#include "cosmology.h"
+#include "utilities.h"
+#include "primordial.h"
+#include "wnw_split.h"
+#include "IR_res.h"
+#include "ps_halo_1loop.h"
+
+
+/** 
+ * Function declarations of main.c module
+ */
+void  initialize();
+void 	cleanup();
+
+
+/**
+* A structure passed to the integrators to hold the parameters fixed in the integration
+*/
+struct integrand_parameters
+{
+	double p1;
+	double p2;
+	double p3;
+	double p4;
+	double p5;
+	double p6;
+	double p7;
+	double p8;
+	double p9;
+	double p10;
+	double p11;
+	long 	p12;
+	long 	p13;
+};
+
+/**
+* Another structure passed to the integrators to hold the parameters fixed in the integration
+*/
+struct integrand_parameters2
+{
+
+	struct Cosmology *p1;
+	struct Cosmology *p2;
+	struct Cosmology *p3;
+
+	double p4;
+	double p5;
+	double p6; 
+	double p7;
+	double p8;
+	double p9;
+	double p10;
+	double p11;
+	double p12;
+
+	long p13;
+	long p14;
+	long p15;
+	long p16;
+	long p17;
+	long p18;
+
+	int p19;
+
+	double *p20;
+	size_t p22;
+};
+	
+
+#endif
+
+
+
+
+
+
+
