c39771a7041e0ca6406492ef1850672daa5a06b3
Dennis Linde <dennis.linde@rwth-aachen.de>
2022-05-31 19:20:59 +0200

    utilities restored func (were moved for testing)

--- external/oneloopeft/utilities.c
+++ external/oneloopeft/utilities.c
@@ -17,11 +17,11 @@
  */
 
 
-//#include "header.h"
-#include <complex.h>
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include "header.h"
+// #include <complex.h>
+// #include <stdio.h>
+// #include <math.h>
+// #include <stdlib.h>
 
 /**
  * Allocate memory to a 1d array of type double and length size
@@ -222,76 +222,149 @@ double *init_1Darray(long n,double xmin,double xmax)
 	}
 
 
-// /**
-//  * initialize a 1d array, with values in the range of [xmin,xmax] and evenely-space on natural-log scale
-//  * 
-//  * @param n     	Input: number of elements
-//  * @param xmin      Input: start point
-//  * @param xmiax     Input: end point
-//  * 
-//  * @return a pointer to a double type 1d array, with values initialized
-//  */
-// double *loginit_1Darray(long n,double xmin,double xmax)
-// {
-
-// 	double inc;
-// 	double *A;
-// 	long i;
-
-// 	A = make_1Darray(n);
-	
-// 	xmin = log(xmin);
-// 	xmax = log(xmax);
-// 	inc  = (xmax-xmin)/n;
-
-// 	for(i=0L;i<n;i++)
-// 	{
-// 		A[i] = xmin + inc * i;
-// 		A[i] = exp(A[i]); 
-// 	}
-
-// 	return A;
-// }
-
-
-// /**
-//  * initialize a 1d array, with values in the range of [xmin,xmax] and evenely-space on log10 scale
-//  * 
-//  * @param n     	Input: number of elements
-//  * @param xmin      Input: start point
-//  * @param xmiax     Input: end point
-//  * 
-//  * @return a pointer to a double type 1d array, with values initialized
-//  */
-// double *log10init_1Darray(long n, double inc, double xmin)
-// {
-	
-// 	double *A, *logA;
-// 	long i;
-
-// 	double logxmin;
-// 	logA = make_1Darray(n);
-// 	A = make_1Darray(n);
-
-// 	logxmin = log10(xmin);
-
-// 	for(i=0L;i<n;i++)
-// 	{
-// 		logA[i] = logxmin + inc * i;
-// 		A[i] = pow(10,logA[i]); 
-// 	}
-
-// 	free(logA);
-
-// 	return A;
-// }
-
-void c_nonprop(double complex* arr1, double complex** matrix, double complex* arr2, int rows, double complex *result)
+/**
+ * initialize a 1d array, with values in the range of [xmin,xmax] and evenely-space on natural-log scale
+ * 
+ * @param n     	Input: number of elements
+ * @param xmin      Input: start point
+ * @param xmiax     Input: end point
+ * 
+ * @return a pointer to a double type 1d array, with values initialized
+ */
+double *loginit_1Darray(long n,double xmin,double xmax)
 {
-	double complex* arr3;
-	arr3 = make_1D_c_array(rows);
-	c_matmul(matrix, arr2, rows, rows, arr3);
-	c_dot(arr1, arr3, rows, result);
+
+	double inc;
+	double *A;
+	long i;
+
+	A = make_1Darray(n);
+	
+	xmin = log(xmin);
+	xmax = log(xmax);
+	inc  = (xmax-xmin)/n;
+
+	for(i=0L;i<n;i++)
+	{
+		A[i] = xmin + inc * i;
+		A[i] = exp(A[i]); 
+	}
+
+	return A;
+}
+
+
+/**
+ * initialize a 1d array, with values in the range of [xmin,xmax] and evenely-space on log10 scale
+ * 
+ * @param n     	Input: number of elements
+ * @param xmin      Input: start point
+ * @param xmiax     Input: end point
+ * 
+ * @return a pointer to a double type 1d array, with values initialized
+ */
+double *log10init_1Darray(long n, double inc, double xmin)
+{
+	
+	double *A, *logA;
+	long i;
+
+	double logxmin;
+	logA = make_1Darray(n);
+	A = make_1Darray(n);
+
+	logxmin = log10(xmin);
+
+	for(i=0L;i<n;i++)
+	{
+		logA[i] = logxmin + inc * i;
+		A[i] = pow(10,logA[i]); 
+	}
+
+	free(logA);
+
+	return A;
+}
+
+/**
+ * Count the number of lines of a file
+ * 
+ * @param fname     	Input: filename
+ * @return long integer value of nlines
+ */
+long count_lines_in_file(char *fname)
+{
+	FILE* ifp;
+	long numlines = 0L;
+	char c;
+	int verbose = 1;
+	char line[MAXL];
+	
+	ifp = fopen(fname, "r");
+	if(ifp == NULL)
+	{
+    	fprintf( stderr, "Unable to open file \n" );
+    	exit( 1 );
+	}
+
+	int numcoms = 0;
+	while((c=fgetc(ifp)) != EOF)
+	{ 
+			if(c =='\n')
+       			numlines++;
+     
+       		if(c =='#')
+       			numcoms++;
+       		    	
+	}
+	if (verbose > 3) printf("Number of lines: %ld Number of comment lines: %ld\n",numlines,numcoms);		
+	
+	int data_len = 	numlines-numcoms;
+
+	fclose(ifp);
+
+	return data_len;
+}
+
+
+/**
+ * Count the number of columns of a file
+ * 
+ * @param fname     	Input: filename
+ * @return long integer value of ncols
+ */
+long count_cols_in_file(char *fname)
+{
+	FILE* ifp;
+	long nrows = 0L, ncols =1;
+	char c;
+	int verbose = 1;
+	char line[MAXL];
+	
+	ifp = fopen(fname, "r");
+	if(ifp == NULL)
+	{
+    	fprintf( stderr, "Unable to open file \n" );
+    	exit( 1 );
+	}
+
+	int count = 0;
+	while(fgets(line, sizeof line, ifp) )
+	{ 
+	 	if(*line == '#')  continue; 
+	 	while((c=fgetc(ifp)) != EOF)
+		{ 
+			if(c =='\n')
+	       		nrows++;
+			if (nrows == 2 && c== '\t')
+				ncols ++;
+		}  
+	} 	
+
+	if (verbose > 3) printf("num_rows: %ld \t num_cols =%ld\n",nrows,ncols);
+	fclose(ifp);
+		
+	return ncols;
 }
 
 void c_matmul(double complex** matrix, double complex* arr, int rows, int cols, double complex* result)
@@ -312,4 +385,12 @@ void c_dot(double complex* arr1, double complex* arr2, int rows, double complex 
 	for (i=0; i<rows; i++){
 		*result += arr1[i] * arr2[i];
 	}
-	}
+}
+
+void c_nonprop(double complex* arr1, double complex** matrix, double complex* arr2, int rows, double complex *result)
+{
+	double complex* arr3;
+	arr3 = make_1D_c_array(rows);
+	c_matmul(matrix, arr2, rows, rows, arr3);
+	c_dot(arr1, arr3, rows, result);
+}
