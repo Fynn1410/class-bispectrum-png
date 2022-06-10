5b85ddf89a39dbba7b0d606f7cf8c415c0e42009
Dennis Linde <dennis.linde@rwth-aachen.de>
2022-06-10 15:14:57 +0200

    vecmul appended

--- external/oneloopeft/utilities.c
+++ external/oneloopeft/utilities.c
@@ -367,24 +367,8 @@ long count_cols_in_file(char *fname)
 	return ncols;
 }
 
-void np_mat_fill(double complex (mat_func)(double complex, double complex), struct fft_struct *fft_input, double k, double complex **matrix)
-{
-	int Nmax = fft_input -> nfft;
 
-	int i,j;
-	double complex nu1, nu2;
-	for (i=0; i<Nmax+1; i++){
-		for (j=i; j<Nmax+1; j++){                        
-				nu1 = -0.5 * fft_input->etam_g[i];
-				nu2 = -0.5 * fft_input->etam_g[j];
-				
-				matrix[i][j] = mat_func(nu1,nu2);
-				matrix[j][i] = mat_func(nu1,nu2);
-		}
-	}
-}
-
-void np_mu_mat_fill(double complex (mat_func)(double complex, double complex, double), struct fft_struct *fft_input, double k, double mu, double complex **matrix)
+void np_mat_fill(double complex (mat_func)(double complex, double complex, double), struct fft_struct *fft_input, double k, double mu, double complex **matrix)
 {
 	int Nmax = fft_input -> nfft;
 
@@ -401,19 +385,7 @@ void np_mu_mat_fill(double complex (mat_func)(double complex, double complex, do
 	}
 }
 
-void p_mat_fill(double complex (mat_func)(double complex), struct fft_struct *fft_input, double k, double complex *matrix)
-{
-	int Nmax = fft_input -> nfft;
-
-	int i;
-	double complex nu1;
-	for (i=0; i<Nmax+1; i++){
-		nu1 = -0.5 * fft_input->etam_g[i];
-		matrix[i] = mat_func(nu1);
-	}
-}
-
-void p_mu_mat_fill(double complex (mat_func)(double complex, double), struct fft_struct *fft_input, double k, double mu, double complex *matrix)
+void p_mat_fill(double complex (mat_func)(double complex, double), struct fft_struct *fft_input, double k, double mu, double complex *matrix)
 {
 	int Nmax = fft_input -> nfft;
 
@@ -443,6 +415,15 @@ void c_matmul(double complex** matrix, double complex* arr, int rows, int cols, 
 		for (j=0; j<cols; j++){
 			result[i] += matrix[i][j] * arr[j];
 		}
+	}
+}
+
+void vecmult(double* arr1, double* arr2, int rows, double *result)
+{
+	int i;
+	
+	for (i=0; i<rows; i++){
+		result[i] = arr1[i] * arr2[i];
 	}
 }
 
