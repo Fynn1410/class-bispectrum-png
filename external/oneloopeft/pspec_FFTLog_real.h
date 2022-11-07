14e3cf5af2efd07db901e39ea97543c29cf83e19
Dennis Linde <dennis.linde@rwth-aachen.de>
2022-11-07 12:15:10 +0100

    Pk_Halo_real adjusted to be bias+counter dependent

--- external/oneloopeft/pspec_FFTLog_real.h
+++ external/oneloopeft/pspec_FFTLog_real.h
@@ -4,5 +4,12 @@ int Real_Oneloop_FFTLog(struct background *pba, struct primordial *ppm, struct f
 int Real_Matter_IR_Resummed(struct background *pba, struct primordial *ppm, struct fourier *pfo,
                  int index_k, double z, long SPLIT, double *pk);
 
-int Real_Galaxy_IR_Resummed(struct background *pba, struct primordial *ppm, struct fourier *pfo,
-                 int index_k, double z, long SPLIT, double *pk);
+int Real_Galaxy_IR_Resummed_default(struct fourier *pfo, struct background *pba, struct primordial *ppm,
+                    int index_k, double z, long SPLIT, double *pk);
+
+int Real_Galaxy_IR_Resummed(struct fourier *pfo, struct background *pba, struct primordial *ppm,
+                    int index_k, double z,
+                    double b1, double b2, double bG2, double btd, double R2,
+                    double cs2,
+                    long SPLIT,
+                    double *pk);
