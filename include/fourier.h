3c3c3df0d7da625b9ab0c0c7eb71376286314460
lesgourg <lesgourg@physik.rwth-aachen.de>
2024-09-16 15:17:28 +0200

    wnw_split donne within new function fourier_wnw_split; still need to move eft_wnw_split to dedicated external module

--- include/fourier.h
+++ include/fourier.h
@@ -491,6 +491,13 @@ extern "C" {
                         double * lnpk_ic
                         );
 
+  int fourier_wnw_split(
+                        struct precision *ppr,
+                        struct background *pba,
+                        struct primordial * ppm,
+                        struct fourier *pfo
+                        );
+
   int fourier_sigmas(
                      struct fourier * pfo,
                      double R,
